830651bf0af1bec7f9f505dd6ac11fbbca22b25e
Aubhro Sengupta <aubhros@gmail.com>
2019-10-26 23:55:01 -0700

    Print user location in realtime after AR vc is launched

--- calhacks-2019/ViewControllers/ARViewController.swift
+++ calhacks-2019/ViewControllers/ARViewController.swift
@@ -11,12 +11,15 @@ import ARCL
 import CoreLocation
 
 
-class ARViewController: UIViewController {
+class ARViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
     var phoneNumber: String = ""
     var username: String = ""
     var ID = ""
     
     var sceneLocationView = SceneLocationView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,23 @@ class ARViewController: UIViewController {
         // Do any additional setup after loading the view.
         sceneLocationView.run()
         view.addSubview(sceneLocationView)
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
     
     override func viewDidLayoutSubviews() {
